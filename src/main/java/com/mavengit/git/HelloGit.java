c96a94e99b5f0f55068e7eda82aab85457f04ec1
Binary-yuan <69451495+Binary-yuan@users.noreply.github.com>
2020-08-10 15:44:56 +0800

    在线更新

--- src/main/java/com/mavengit/git/HelloGit.java
+++ src/main/java/com/mavengit/git/HelloGit.java
@@ -4,13 +4,14 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		System.out.println("更新1");
-		System.out.println("创建分支");		
+		System.out.println("鏇存柊1");
+		System.out.println("鍒涘缓鍒嗘敮");		
 <<<<<<< HEAD
-		System.out.println("主干添加");		
+		System.out.println("涓诲共娣诲姞");		
 =======
-		System.out.println("分支添加");		
+		System.out.println("鍒嗘敮娣诲姞");		
 >>>>>>> refs/heads/dev
+		System.out.println("鍦ㄧ嚎娣诲姞");
 
 	}
 
